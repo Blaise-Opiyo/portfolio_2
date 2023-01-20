bce5e502cf27c779fd321280ea70d5cdbca6551b
Blaise-Opiyo <blaiseopiyo@gmail.com>
2023-01-20 12:45:02 +0300

    added error message to contact form

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -9,19 +9,20 @@ const ContactForm = () =>{
 //   if (state.succeeded) {
 //       return <p>Thanks for joining!</p>;
 //   }
-    const [success, setSuccess] = useState(false);
+    const [success, setSuccess] = useState("");
 
     const contactForm = useRef();
 
     const sendEmail = (e) => {
         e.preventDefault();
-        setSuccess(false);
+        setSuccess("");
         emailjs.sendForm(process.env.REACT_APP_YOUR_SERVICE_ID, process.env.REACT_APP_YOUR_TEMPLATE_ID, contactForm.current, process.env.REACT_APP_YOUR_PUBLIC_KEY)
         .then((result) => {
-            console.log(result.text);
+            // console.log(result.text);
             setSuccess(true);
         }, (error) => {
-            console.log(error.text);
+            // console.log(error.text);
+            setSuccess(false);
         });
     };
     const successMsg = {
@@ -34,10 +35,23 @@ const ContactForm = () =>{
         textAlign: "center",
         color: "rgba(75,181,67, 1)"
     }
+    const errorMsg = {
+        width: "fit-content",
+        width: "-moz-fit-content",
+        height: "fit-content",
+        height: "-moz-fit-content",
+        marginTop: "10px",
+        marginBottom: "10px",
+        textAlign: "center",
+        color: "rgba(243,32,19,1)"
+    }
     return(
         <form ref={contactForm} onSubmit={sendEmail} method="POST">
             {(success === true ? 
             <p style={successMsg}>Email sent successfully</p> :
+            null)}
+            {(success === false ? 
+            <p style={errorMsg}>An error occurred and email not sent</p> :
             null)}
             <input type="text" id="fullname" name="sender_name" placeholder="Your name"/>
             {/* <ValidationError 
